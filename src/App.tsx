a6dad235084f1bc375998b2154984c566efb06ce
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-06-29 17:06:47 +0500

    moved dragonslist to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
 import './App.css';
-import { DragonCardType } from './components/Card';
-import DragonList from './components/List';
+import Sidebar from './components/SideBar';
 
-const dragonsList: DragonCardType[] = [
-  {
-    name: 'Adult Black Dragon',
-    description: 'Huge black dragon',
-    languages: 'Common, Draconic',
-    imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
-  },
-  {
-    name: 'Adult Black Dragon',
-    description: 'Huge black dragon',
-    languages: 'Common, Draconic',
-    imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
-  },
-  {
-    name: 'Adult Black Dragon',
-    description: 'Huge black dragon',
-    languages: 'Common, Draconic',
-    imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
-  },
-  {
-    name: 'Adult Black Dragon',
-    description: 'Huge black dragon',
-    languages: 'Common, Draconic',
-    imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
-  },
-  {
-    name: 'Adult Black Dragon',
-    description: 'Huge black dragon',
-    languages: 'Common, Draconic',
-    imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
-  },
-]
 
 function App() {
   return (
-    <DragonList dragons={dragonsList}/>
+    <>
+      <Sidebar />
+    </>
   );
 }
 
---
+++ src/data/dragonsList.ts
@@ -0,0 +1,39 @@
+import { DragonCardType } from "../components/Card";
+
+export const dragonsList: DragonCardType[] = [
+  {
+    name: 'Adult Black Dragon',
+    description: 'Huge black dragon',
+    languages: 'Common, Draconic',
+    imgSrc: '/api/images/monsters/adult-black-dragon.png',
+    link: '/api/monsters/adult-black-dragon'
+  },
+  {
+    name: 'Adult Black Dragon',
+    description: 'Huge black dragon',
+    languages: 'Common, Draconic',
+    imgSrc: '/api/images/monsters/adult-black-dragon.png',
+    link: '/api/monsters/adult-black-dragon'
+  },
+  {
+    name: 'Adult Black Dragon',
+    description: 'Huge black dragon',
+    languages: 'Common, Draconic',
+    imgSrc: '/api/images/monsters/adult-black-dragon.png',
+    link: '/api/monsters/adult-black-dragon'
+  },
+  {
+    name: 'Adult Black Dragon',
+    description: 'Huge black dragon',
+    languages: 'Common, Draconic',
+    imgSrc: '/api/images/monsters/adult-black-dragon.png',
+    link: '/api/monsters/adult-black-dragon'
+  },
+  {
+    name: 'Adult Black Dragon',
+    description: 'Huge black dragon',
+    languages: 'Common, Draconic',
+    imgSrc: '/api/images/monsters/adult-black-dragon.png',
+    link: '/api/monsters/adult-black-dragon'
+  },
+]
