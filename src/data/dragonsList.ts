74a33b7727d88fa4dadf4381eaf7b4b93838f995
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-06-29 20:23:10 +0500

    edited dragonslist

--- src/data/dragonsList.ts
+++ src/data/dragonsList.ts
@@ -2,38 +2,38 @@ import { DragonCardType } from "../components/Card";
 
 export const dragonsList: DragonCardType[] = [
   {
-    name: 'Adult Black Dragon',
+    name: 'Adult Black Dragon1',
     description: 'Huge black dragon',
     languages: 'Common, Draconic',
     imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
+    link: '/api/monsters/adult-black-dragon1'
   },
   {
-    name: 'Adult Black Dragon',
+    name: 'Adult Black Dragon2',
     description: 'Huge black dragon',
     languages: 'Common, Draconic',
     imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
+    link: '/api/monsters/adult-black-dragon2'
   },
   {
-    name: 'Adult Black Dragon',
+    name: 'Adult Black Dragon3',
     description: 'Huge black dragon',
     languages: 'Common, Draconic',
     imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
+    link: '/api/monsters/adult-black-dragon3'
   },
   {
-    name: 'Adult Black Dragon',
+    name: 'Adult Black Dragon4',
     description: 'Huge black dragon',
     languages: 'Common, Draconic',
     imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
+    link: '/api/monsters/adult-black-dragon4'
   },
   {
-    name: 'Adult Black Dragon',
+    name: 'Adult Black Dragon5',
     description: 'Huge black dragon',
     languages: 'Common, Draconic',
     imgSrc: '/api/images/monsters/adult-black-dragon.png',
-    link: '/api/monsters/adult-black-dragon'
+    link: '/api/monsters/adult-black-dragon5'
   },
 ]
